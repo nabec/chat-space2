388759c3047b578a428adda05a1bca93a83b530b
nabec <nabec.48@gmail.com>
2019-10-26 14:20:46 +0900

    fix password and password_confirmation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
 
   factory :user do
+    password = Faker::Internet.password
     name {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password {Faker::Internet.password}
-    password_confirmation {Faker::Internet.password}
+    password {password}
+    password_confirmation {password}
   end
 
 end
